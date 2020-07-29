22830640b40a1200325cd17e61a6660f1b1a59a6
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-07-29 22:29:17 +0800

    Update preload.sh

--- preload.sh
+++ preload.sh
@@ -8,7 +8,7 @@ yum remove -y kernel-devel
 
 rpm --import https://www.elrepo.org/RPM-GPG-KEY-elrepo.org
 
-rpm -Uvh https://github.com/HXHGTS/WireGuardServer/raw/master/kernel-ml-devel-5.7.8-1.el7.elrepo.x86_64.rpm
+rpm -Uvh https://github.com/HXHGTS/WireGuardServer/raw/master/elrepo-release-7.0-5.el7.elrepo.noarch.rpm
 
 yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
 
@@ -18,7 +18,7 @@ sed -i "s/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/" /etc/default/grub
 
 grub2-mkconfig -o /boot/grub2/grub.cfg
 
-wget https://github.com/HXHGTS/WireGuardServer/raw/master/elrepo-release-7.0-5.el7.elrepo.noarch.rpm
+wget https://github.com/HXHGTS/WireGuardServer/raw/master/kernel-ml-devel-5.7.8-1.el7.elrepo.x86_64.rpm
 
 rpm -ivh elrepo-release-7.0-5.el7.elrepo.noarch.rpm
 
