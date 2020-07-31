6262ee0cdca3aacebf1f87064a7e405c10c02c65
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-07-31 13:38:41 +0800

    Update preload.sh

--- preload.sh
+++ preload.sh
@@ -14,14 +14,10 @@ rpm -Uvh https://www.elrepo.org/elrepo-release-7.el7.elrepo.noarch.rpm
 
 yum --disablerepo="*" --enablerepo="elrepo-kernel" list available
 
-yum -y --enablerepo=elrepo-kernel install kernel-ml
+yum --enablerepo=elrepo-kernel install kernel-ml-devel -y
 
 sed -i "s/GRUB_DEFAULT=saved/GRUB_DEFAULT=0/" /etc/default/grub
 
 grub2-mkconfig -o /boot/grub2/grub.cfg
-
-rpm -ivh elrepo-release-7.el7.elrepo.noarch.rpm
-
-yum --enablerepo=elrepo-kernel install kernel-ml-devel -y
 
 reboot
