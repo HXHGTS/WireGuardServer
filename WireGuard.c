6ab3e9c41a7732d3271209b314721b7c7338d9da
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2020-08-21 19:24:16 +0800

    Update WireGuard.c

--- WireGuard.c
+++ WireGuard.c
@@ -13,7 +13,7 @@ int DNS(){
         system("clear");
         printf("正在配置DNS. . .\n");
         if (region == 1) {
-            system("wget https://github.com/HXHGTS/AdguardHomeInstall/raw/master/CN.sh -O install.sh");
+            system("wget https://github.com/HXHGTS/AdguardHomeInstall/raw/master/install.sh -O install.sh");
             system("sudo bash install.sh");
             printf("默认dns已被设置为清华大学+中科大DNS!\n");
             server_info = fopen("/etc/wireguard/dns.info", "w");
