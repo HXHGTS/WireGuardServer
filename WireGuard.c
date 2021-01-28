80f7c2ae54ad636a1b414d6d7fb4849898a5a5ef
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-01-28 13:41:24 +0800

    Update WireGuard.c

--- WireGuard.c
+++ WireGuard.c
@@ -23,7 +23,7 @@ int DNS(){
     fprintf(server_info, "resolv-file=/etc/resolv.dnsmasq.conf\n");
     fprintf(server_info, "strict-order\n");
     fprintf(server_info, "addn-hosts=/etc/dnsmasq.hosts\n");
-    fprintf(server_info, "listen-address=10.0.0.1, fd00:7::1\n");
+    fprintf(server_info, "listen-address=10.0.0.1\n");
     fclose(server_info);//使用系统默认DNS解析
     server_info = fopen("/etc/resolv.dnsmasq.conf", "w");
     fprintf(server_info, "nameserver %s\n",dns_server);
@@ -220,7 +220,7 @@ int AddUser() {
     client_config = fopen(FileName, "a");
     fprintf(client_config, "Address = 10.0.0.%d/32, fd00:7::%d/128\n",num,num);
     fprintf(client_config, "MTU = 1420\n");//
-    fprintf(client_config, "DNS = 10.0.0.1,fd00:7::1\n");
+    fprintf(client_config, "DNS = 10.0.0.1\n");
     fprintf(client_config, "\n[Peer]\n");
     fprintf(client_config, "AllowedIPs = 0.0.0.0/0,::/0\n");
     fprintf(client_config, "Endpoint = %s:%d\n",ServerName,ListenPort);
