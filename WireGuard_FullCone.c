6dd9d96ccca75540da453a836bfda6c63de581b6
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-02-17 18:26:50 +0800

    Update WireGuard_FullCone.c

--- WireGuard_FullCone.c
+++ WireGuard_FullCone.c
@@ -132,6 +132,7 @@ int InstallWireGuard(){
     system("yum install iptables-services -y");
     system("curl -o /etc/yum.repos.d/jdoss-wireguard-epel-7.repo https://cdn.jsdelivr.net/gh/HXHGTS/WireGuardServer/jdoss-wireguard-epel-7.repo");
     system("yum install epel-release wireguard-dkms wireguard-tools qrencode -y");
+    printf("正在编译安装fullcone iptables. . . . . .\n");
     printf("正在生成服务器配置. . . . . .\n");
     server_config = fopen("/etc/wireguard/wg0.conf", "w");
     fprintf(server_config, "[Interface]\n");
@@ -140,10 +141,10 @@ int InstallWireGuard(){
     system("wg genkey | tee /etc/wireguard/server_privatekey | wg pubkey > /etc/wireguard/server_publickey");
     system("cat /etc/wireguard/server_privatekey >> /etc/wireguard/wg0.conf");//服务器私钥，不要修改
     server_config = fopen("/etc/wireguard/wg0.conf", "a");
-    fprintf(server_config, "Address = 10.0.0.1/32, fd00:7::1/128\n");//服务器ip地址，修改需要同时修改客户端配置
+    fprintf(server_config, "Address = 10.0.0.1/32\n");//服务器ip地址，修改需要同时修改客户端配置
     fprintf(server_config, "MTU = 1420\n");
-    fprintf(server_config, "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE;ip6tables -A FORWARD -i wg0 -j ACCEPT; ip6tables -A FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -A POSTROUTING -o eth0 -j MASQUERADE; service iptables save; service ip6tables save\n");//服务器防火墙配置
-    fprintf(server_config, "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j MASQUERADE;ip6tables -D FORWARD -i wg0 -j ACCEPT; ip6tables -D FORWARD -o wg0 -j ACCEPT; ip6tables -t nat -D POSTROUTING -o eth0 -j MASQUERADE; service iptables save; service ip6tables save\n");//服务器防火墙配置
+    fprintf(server_config, "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j FULLCONENAT;service iptables save\n");//服务器防火墙配置
+    fprintf(server_config, "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j FULLCONENAT;service iptables save\n");//服务器防火墙配置
     fprintf(server_config, "ListenPort = %d\n",ListenPort);//服务器监听端口
     fclose(server_config);
     system("rm -f /etc/wireguard/server_privatekey");
@@ -191,7 +192,7 @@ int AddUser() {
     sprintf(command,"cat /etc/wireguard/%s_publickey >> /etc/wireguard/wg0.conf",username);//客户端公钥，不要修改
     system(command);
     server_config = fopen("/etc/wireguard/wg0.conf", "a");
-    fprintf(server_config, "AllowedIPs = 10.0.0.%d/32, fd00:7::%d/128\n",num,num);//客户端ip地址分配，不要修改
+    fprintf(server_config, "AllowedIPs = 10.0.0.%d/32\n",num);//客户端ip地址分配，不要修改
     fprintf(server_config, "PresharedKey = ");
     fclose(server_config); 
     system("cat /etc/wireguard/psk >> /etc/wireguard/wg0.conf");//预共享密钥，不要修改
@@ -213,11 +214,11 @@ int AddUser() {
     sprintf(command, "cat /etc/wireguard/%s_privatekey >> /etc/wireguard/%s.conf", username,username);
     system(command);
     client_config = fopen(FileName, "a");
-    fprintf(client_config, "Address = 10.0.0.%d/32, fd00:7::%d/128\n",num,num);
+    fprintf(client_config, "Address = 10.0.0.%d/32\n",num);
     fprintf(client_config, "MTU = 1420\n");//
-    fprintf(client_config, "DNS = 10.0.0.1, fd00:7::1\n");
+    fprintf(client_config, "DNS = 10.0.0.1\n");
     fprintf(client_config, "\n[Peer]\n");
-    fprintf(client_config, "AllowedIPs = 0.0.0.0/0,::/0\n");
+    fprintf(client_config, "AllowedIPs = 0.0.0.0/0\n");
     fprintf(client_config, "Endpoint = %s:%d\n",ServerName,ListenPort);
     fprintf(client_config, "PublicKey = ");
     fclose(client_config);
