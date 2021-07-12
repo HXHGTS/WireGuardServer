037ba3a0fb3b16d00d272a92fc1b71955f8c338f
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2021-07-12 16:30:43 +0800

    Update WireGuard_FullCone.c

--- WireGuard_FullCone.c
+++ WireGuard_FullCone.c
@@ -136,7 +136,7 @@ int InstallWireGuard(){
     system("cat /etc/wireguard/server_privatekey >> /etc/wireguard/wg0.conf");//服务器私钥，不要修改
     server_config = fopen("/etc/wireguard/wg0.conf", "a");
     fprintf(server_config, "Address = 10.0.0.1/32\n");//服务器ip地址，修改需要同时修改客户端配置
-    fprintf(server_config, "MTU = 1280\n");
+    fprintf(server_config, "MTU = 1500\n");
     fprintf(server_config, "PostUp = PostUp = iptables -I FORWARD -i wg0 -j ACCEPT; iptables -I FORWARD -o wg0 -j ACCEPT;service iptables save\n");//服务器防火墙配置
     fprintf(server_config, "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT;service iptables save\n");//服务器防火墙配置
     fprintf(server_config, "ListenPort = %d\n",ListenPort);//服务器监听端口
@@ -209,7 +209,7 @@ int AddUser() {
     system(command);
     client_config = fopen(FileName, "a");
     fprintf(client_config, "Address = 10.0.0.%d/32\n",num);
-    fprintf(client_config, "MTU = 1280\n");//
+    fprintf(client_config, "MTU = 1500\n");//
     fprintf(client_config, "DNS = 10.0.0.1\n");
     fprintf(client_config, "\n[Peer]\n");
     fprintf(client_config, "AllowedIPs = 0.0.0.0/0\n");
