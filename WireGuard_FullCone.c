3741bba0aa4dac4e5f6b3dc519058eb41fa2c78a
HXHGTS <38959826+HXHGTS@users.noreply.github.com>
2022-07-22 12:30:02 +0800

    Update WireGuard_FullCone.c

--- WireGuard_FullCone.c
+++ WireGuard_FullCone.c
@@ -134,8 +134,8 @@ int InstallWireGuard(){
     system("cat /etc/wireguard/server_privatekey >> /etc/wireguard/wg0.conf");//服务器私钥，不要修改
     server_config = fopen("/etc/wireguard/wg0.conf", "a");
     fprintf(server_config, "Address = 10.0.0.1/32\n");//服务器ip地址，修改需要同时修改客户端配置
-    fprintf(server_config, "PostUp = iptables -t nat -A POSTROUTING -o eth0 -j FULLCONENAT; iptables -t nat -A PREROUTING -i eth0 -j FULLCONENAT\n");//服务器防火墙配置
-    fprintf(server_config, "PostDown = iptables -F; iptables -Z\n");//服务器防火墙配置
+    fprintf(server_config, "PostUp = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o eth0 -j FULLCONENAT; iptables -t nat -A PREROUTING -o eth0 -j FULLCONENAT\n");//服务器防火墙配置
+    fprintf(server_config, "PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o eth0 -j FULLCONENAT; iptables -t nat -D PREROUTING -o eth0 -j FULLCONENAT\n");//服务器防火墙配置
     fprintf(server_config, "ListenPort = %d\n",ListenPort);//服务器监听端口
     fclose(server_config);
     system("rm -f /etc/wireguard/server_privatekey");
